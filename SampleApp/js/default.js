19a1686d5e6e45e8608a4c587b65367cdc712dfe
jsnmc <jsnmcdev@gmail.com>
2013-04-01 01:06:13 -0400

    Initial pass through

---
+++ SampleApp/js/default.js
@@ -0,0 +1,144 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize
+                // your application here.
+            } else {
+                // TODO: This application has been reactivated from suspension.
+                // Restore application state here.
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state
+        // that needs to persist across suspensions here. You might use the
+        // WinJS.Application.sessionState object, which is automatically
+        // saved and restored across suspension. If you need to complete an
+        // asynchronous operation before your application is suspended, call
+        // args.setPromise().
+    };
+
+    app.start();
+})();
+
+var ex
+function basics1() {
+
+    var contents = $('#output');  //returns a jQuery Object
+    //document.getElementById('output').innerHTML =
+    contents[0].innerHTML =
+        SampleComponent.Example.getAnswer();
+
+    ex = new SampleComponent.Example();
+
+    document.getElementById('output').innerHTML += "<br/>" +
+        ex.sampleProperty;
+}
+
+function basics2() {
+    ex.sampleProperty += 1;
+    document.getElementById('output').innerHTML += "<br/>" +
+        ex.sampleProperty;
+}
+
+var propertysetstats
+function runtime1() {
+    document.getElementById('output').innerHTML = "";
+
+    propertysetstats = new SampleComponent.PropertySetStats();
+    var propertyset = propertysetstats.propertySet;
+
+    propertyset.addEventListener("mapchanged", onMapChanged);
+
+    propertyset.insert("FirstProperty", "First property value");
+    propertyset.insert("SuperfluousProperty", "Unnecessary property value");
+    propertyset.insert("AnotherProperty", "A property value");
+
+    propertyset.insert("SuperfluousProperty", "Altered property value")
+    propertyset.remove("SuperfluousProperty");
+
+    document.getElementById('output').innerHTML +=
+        propertysetstats.displayStats();
+}
+
+function onMapChanged(change) {
+    var result
+    switch (change.collectionChange) {
+        case Windows.Foundation.Collections.CollectionChange.reset:
+            result = "All properties cleared";
+            break;
+        case Windows.Foundation.Collections.CollectionChange.itemInserted:
+            result = "Inserted " + change.key + ": '" +
+                change.target.lookup(change.key) + "'";
+            break;
+        case Windows.Foundation.Collections.CollectionChange.itemRemoved:
+            result = "Removed " + change.key;
+            break;
+        case Windows.Foundation.Collections.CollectionChange.itemChanged:
+            result = "Changed " + change.key + " to '" +
+                change.target.lookup(change.key) + "'";
+            break;
+    }
+
+    document.getElementById('output').innerHTML +=
+        "<br/>" + result;
+}
+
+function runtime2() {
+    try {
+        propertysetstats.addMore();
+    }
+    catch (ex) {
+        document.getElementById('output').innerHTML +=
+            "<br/><b>" + ex + "</b>";
+    }
+
+    document.getElementById('output').innerHTML +=
+        propertysetstats.displayStats();
+}
+
+var names
+
+function returns1() {
+    names = SampleComponent.Example.getMapOfNames();
+    document.getElementById('output').innerHTML = showMap(names);
+}
+
+ct = 7
+
+function returns2() {
+    if (!names.hasKey(17)) {
+        names.insert(43, "forty-three");
+        names.insert(17, "seventeen");
+    }
+    else {
+        var err = names.insert("7", ct++);
+        names.insert("forty", "forty");
+    }
+    document.getElementById('output').innerHTML = showMap(names);
+}
+
+function showMap(map) {
+    var item = map.first();
+    var retval = "<ul>";
+
+    for (var i = 0, len = map.size; i < len; i++) {
+        retval += "<li>" + item.current.key + ": " + item.current.value + "</li>";
+        item.moveNext();
+    }
+    return retval + "</ul>";
+}
+
+
---
+++ WindowsRuntimeComponent1/SampleComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleComponent
+{
+    public sealed class Example
+    {
+        public static string GetAnswer()
+        {
+            return "The answer is 42.";
+        }
+
+        public int SampleProperty { get; set; }
+
+        public static IDictionary<int, string> GetMapOfNames()
+        {
+            Dictionary<int, string> retval = new Dictionary<int, string>();
+            retval.Add(1, "one");
+            retval.Add(2, "two");
+            retval.Add(3, "three");
+            retval.Add(42, "forty-two");
+            retval.Add(100, "one hundred");
+            return retval;
+        }
+    }
+}
